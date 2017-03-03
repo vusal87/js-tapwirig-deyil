8f9059369f281d8a117de19ff30b27b26f9452b5
vusal <vusalhaciyev9@gmail.com>
2017-03-03 09:02:25 +0400

    pa

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.getElementsByTagName("p")[0].innerHTML=1
+// document.getElementsByTagName("p")[1].innerHTML=2
+// document.getElementsByTagName("p")[2].innerHTML=3
+// document.getElementsByTagName("p")[3].innerHTML=4
+// document.getElementsByTagName("p")[4].innerHTML=5
+// var =document.getElementsByClassName("main");
+
+// for(i=0;i<4;i++){
+	document.getElementsByClassName("main")[1].innerHTML="inci";
+// }
+
